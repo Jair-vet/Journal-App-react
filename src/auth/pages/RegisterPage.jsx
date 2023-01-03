9cc449742b445eba9f650cb66758864c0af5ac88
Jair <jairaceves56@gmail.com>
2023-01-03 11:07:22 -0600

    Pagina de Registro Lista

--- src/auth/pages/RegisterPage.jsx
+++ src/auth/pages/RegisterPage.jsx
@@ -1,8 +1,69 @@
+import { Link as RouterLink } from 'react-router-dom';
+import { Google } from "@mui/icons-material"
+import { Button, Grid, Link, TextField, Typography } from "@mui/material"
+import { AuthLayout } from '../layout/AuthLayout';
 
 export const RegisterPage = () => {
   return (
-    <>
-        RegisterPage
-    </>
+    <AuthLayout title='Register' >
+      <form>
+        <Grid container>
+          {/* Nombre Completo */}
+          <Grid item xs={ 12 } sx={{ mt: 2 }}>
+            <TextField 
+              label="Nombre Completo" 
+              type="text" 
+              placeholder="Pablo escobar"
+              fullWidth
+            />
+          </Grid>
+
+          {/* Correo */}
+          <Grid item xs={ 12 } sx={{ mt: 2 }}>
+            <TextField 
+              label="Correo" 
+              type="email" 
+              placeholder="correo@correo.com"
+              fullWidth
+            />
+          </Grid>
+
+          {/* Contraseña */}
+          <Grid item xs={ 12 } sx={{ mt: 2 }}>
+            <TextField 
+              label="Contraseña" 
+              type="password" 
+              placeholder="contraseña"
+              fullWidth
+            />
+          </Grid>
+
+          {/* Botones */}
+          <Grid container spacing={ 2 } sx={{ mb: 2, mt: 1 }}>
+            <Grid item xs={ 12 }>
+              <Button variant="contained" fullWidth >
+                Crear Cuenta
+              </Button>
+            </Grid>
+          </Grid>
+
+          {/* Cuenta/ Registro */}
+          <Grid container direction='row' justifyContent='end'>
+            <Typography sx={{ mr: 1, textDecoration: 'none' }}>¿Ya tienes Cuenta?</Typography>
+            <Link 
+              component={ RouterLink }
+              color='inherit' 
+              to="/auth/login"
+            >
+              Ingresar
+            </Link>
+
+          </Grid>
+
+        </Grid>
+      </form>
+    </AuthLayout>
+
   )
 }
+
