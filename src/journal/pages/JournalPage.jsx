d461af8ea6c613a9659b1c49664b3d1e00ddca13
Jair <jairaceves56@gmail.com>
2023-01-03 12:27:01 -0600

    Agregamos la apariencia

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,9 +1,15 @@
 import { Typography } from "@mui/material"
+import { JournalLayout } from "../layout/JournalLayout"
+import { NothingSelectedView } from "../views/NothingSelectedView"
 
 export const JournalPage = () => {
   return (
-    <>
-      <Typography variant='h1'>JournalPage</Typography>
-    </>
+    <JournalLayout>
+
+      {/* <Typography >Lorem ipsum ipsum atque exercitationem vero at architecto maiores.</Typography> */}
+      {/* No hay nada seleccionado */}
+      <NothingSelectedView />
+
+    </JournalLayout>
   )
 }
