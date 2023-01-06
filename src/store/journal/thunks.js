cc4a3621266dbaff17096e514db790493fb8bf02
Jair <jairaceves56@gmail.com>
2023-01-06 12:03:47 -0600

    mostramos las notas en el state

--- src/store/journal/thunks.js
+++ src/store/journal/thunks.js
@@ -1,8 +1,8 @@
 import { collection, /* deleteDoc, */ doc, setDoc } from 'firebase/firestore/lite';
 import { FirebaseDB } from '../../firebase/config';
 import { addNewEmptyNote, setActiveNote } from './';
-import { savingNewNote } from './journalSlice';
-// import { fileUpload, loadNotes } from '../../helpers';
+import { savingNewNote, setNotes } from './journalSlice';
+import { /* fileUpload, */ loadNotes } from '../../helpers';
 
 
 export const startNewNote = () => {
@@ -27,5 +27,16 @@ export const startNewNote = () => {
         dispatch( addNewEmptyNote( newNote ) );
         dispatch( setActiveNote( newNote ) );
 
+    }
+}
+
+export const startLoadingNotes = () => {
+    return async( dispatch, getState ) => {
+        
+        const { uid } = getState().auth;
+        if ( !uid ) throw new Error('El UID del usuario no existe');
+
+        const notes = await loadNotes( uid );
+        dispatch( setNotes( notes ) );
     }
 }
