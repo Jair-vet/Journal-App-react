38f9f7ff0d52d06f0e247295a3e56bca5f4ca356
Jair <jairaceves56@gmail.com>
2023-01-06 11:36:14 -0600

    mostramos y activamos al dar click en el boton mas

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,22 +1,34 @@
-import { AddOutlined } from "@mui/icons-material"
-import { IconButton, Typography } from "@mui/material"
-import { JournalLayout } from "../layout/JournalLayout"
-import { NoteView } from "../views/NoteView"
-import { NothingSelectedView } from "../views/NothingSelectedView"
+import { useDispatch, useSelector } from 'react-redux';
+import { IconButton } from '@mui/material';
+import { AddOutlined } from '@mui/icons-material';
+
+import { JournalLayout } from '../layout/JournalLayout';
+import { NoteView, NothingSelectedView } from '../views';
+import { startNewNote } from '../../store/journal/thunks';
 
 export const JournalPage = () => {
+
+  const dispatch = useDispatch();
+  const { isSaving, active } = useSelector( state => state.journal );
+
+  const onClickNewNote = () => {
+    dispatch( startNewNote() );
+  }
+
+  
   return (
     <JournalLayout>
 
-      {/* <Typography >Lorem ipsum ipsum atque exercitationem vero at architecto maiores.</Typography> */}
-     
-      {/* No hay nada seleccionado */}
-      <NothingSelectedView />
-
-      <NoteView />
+      {
+        (!!active)
+          ? <NoteView />
+          : <NothingSelectedView />
+      }
 
       <IconButton 
+        onClick={ onClickNewNote }
         size="large"
+        disabled={ isSaving }
         sx={{
           color: 'white',
           backgroundColor: 'error.main',
